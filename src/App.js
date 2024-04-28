a53412dd25bcfa306f637dcf92e2d0f918c0ba64
GiovanaHoffmann <giovana.hoffmann@hotmail.com>
2024-04-28 06:49:17 -0300

    add icon e att de App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import FormWrapper from './components/form';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} alt="Logo" className="App-logo" />
         <h1>Nobel Prize Winners</h1>
+        <img src={logo} alt="Logo" className="App-logo" />
       </header>
       <FormWrapper />
     </div>
